b38402cc8340f93f47510d876695a7645744531d
briankmbugua <briankmbuguak@gmail.com>
2023-04-13 13:30:10 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+"""This module has a base class for all models"""
 import uuid
 from datetime import datetime
 class BaseModel:
+    """The base class for all models"""
     def __init__(self):
-        self.id = f"{uuid.uuid4()}"
+        """Instantiates a new model"""
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def save(self):
+        """ updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
     def to_dict(self):
-        dict_ = self.__dict__.copy()
-        dict_['__class__'] = self.__class__.__name__
-        dict_['created_at'] = self.created_at.isoformat()
-        dict_['updated_at'] = self.updated_at.isoformat()
-        return dict_
+        """This converts the instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
 
-
-
-myobj = BaseModel()
-print(myobj.__dict__)
-print("dict")
-print(myobj.to_dict())
 
